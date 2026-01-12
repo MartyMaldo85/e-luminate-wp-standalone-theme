2307bf1d2ca8059ec3d118091a48ab41678bd41c
Nazario A. Ayala <nazario@niztech.com>
2026-01-12 14:24:51 -0800

    Added footer logo

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -1,19 +1,28 @@
+<?php
+$logo = implode( DIRECTORY_SEPARATOR, array( get_template_directory_uri(), 'assets', 'soroptimist-logo.svg' ) )
+?>
+
 <footer class="layout-footer">
-	Soroptimist International of Novato &copy; <?php echo gmdate( 'Y' ); ?>
-	<?php
-	wp_nav_menu(
-		array(
-			'theme_location' => 'social',
-			'container'      => false,
-			'menu'           => 'social',
-		)
-	);
-	wp_nav_menu(
-		array(
-			'theme_location' => 'footer',
-			'container'      => false,
-			'menu'           => 'footer',
-		)
-	);
-	?>
+	<img src="<?php echo $logo; ?>" alt="" width="37" height="66" class="sorop-logo"/>
+	<div>
+		<p>Soroptimist International of Novato &copy; <?php echo gmdate( 'Y' ); ?></p>
+		<?php
+		wp_nav_menu(
+			array(
+				'class'          => 'social',
+				'theme_location' => 'social',
+				'container'      => false,
+				'menu'           => 'social',
+			)
+		);
+		wp_nav_menu(
+			array(
+				'class'          => 'footer',
+				'theme_location' => 'footer',
+				'container'      => false,
+				'menu'           => 'footer',
+			)
+		);
+		?>
+	</div>
 </footer>

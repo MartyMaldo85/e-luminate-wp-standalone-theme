c5bb6f0ebf209add8e46c22c05d14009b180770e
Nazario A. Ayala <nazario@niztech.com>
2026-01-12 08:56:12 -0800

    Pass at menu redesign

--- template-parts/layout-nav.php
+++ template-parts/layout-nav.php
@@ -15,54 +15,59 @@
 	}
 </script>
 
-<nav class="body-nav" id="body-nav" data-selected="">
-	<button class="btn btn-main-menu list-in" onclick="toggle('list-in')">Shows</button>
-	<button class="btn btn-main-menu about-us" onclick="toggle('about-us')">About Us</button>
-</nav>
+<div class="body-nav" id="body-nav" data-selected="">
+	<nav>
+		<button class="btn btn-main-menu list-in" onclick="toggle('list-in')"><?php echo __( 'Shows', 'eluminate-standalone' ); ?></button>
+		<button class="btn btn-main-menu about-us" onclick="toggle('about-us')"><?php echo __( 'About Us', 'eluminate-standalone' ); ?></button>
+	</nav>
 
-<div id="body-sub-nav">
-	<div class="about-us menu">
-		<section>
-			<?php wp_nav_menu(
-				array(
-					'container'      => false,
-					'menu'           => 'about-us',
-					'menu_class'     => 'menu-list',
-					'menu_id'        => '',
-					'theme_location' => 'about-us',
-				)
-			); ?>
-		</section>
-	</div>
-	<div class="list-in menu">
-		<section class="shows">
-			<h2 class="title">View</h2>
-			<?php
-			wp_nav_menu(
-				array(
-					'container'      => false,
-					'menu'           => 'shows',
-					'menu_class'     => 'menu-list',
-					'menu_id'        => '',
-					'theme_location' => 'shows',
-				)
-			);
-			?>
-		</section>
-		<section class="topic">
-			<h2 class="title">By topic</h2>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'list-in',
-					'container'      => false,
-					'menu_class'     => 'menu-list',
-					'menu_id'        => '',
-					'menu'           => 'list-in',
-				)
-			);
-			?>
-		</section>
+	<div id="body-sub-nav">
+		<div class="about-us menu">
+			<h2 class="title"><?php echo __( 'Page', 'eluminate-standalone' ); ?></h2>
+			<section>
+				<?php
+				wp_nav_menu(
+					array(
+						'container'      => false,
+						'menu'           => 'about-us',
+						'menu_class'     => 'menu-list',
+						'menu_id'        => '',
+						'theme_location' => 'about-us',
+					)
+				);
+				?>
+			</section>
+		</div>
+		<div class="list-in menu">
+			<section class="shows">
+				<h2 class="title"><?php echo __( 'View', 'eluminate-standalone' ); ?></h2>
+				<?php
+				wp_nav_menu(
+					array(
+						'container'      => false,
+						'menu'           => 'shows',
+						'menu_class'     => 'menu-list',
+						'menu_id'        => '',
+						'theme_location' => 'shows',
+					)
+				);
+				?>
+			</section>
+			<section class="topic">
+				<h2 class="title"><?php echo __( 'By Topic', 'eluminate-standalone' ); ?></h2>
+				<?php
+				wp_nav_menu(
+					array(
+						'container'      => false,
+						'menu'           => 'list-in',
+						'menu_class'     => 'menu-list',
+						'menu_id'        => '',
+						'theme_location' => 'list-in',
+					)
+				);
+				?>
+			</section>
+		</div>
 	</div>
 </div>
 <?php
